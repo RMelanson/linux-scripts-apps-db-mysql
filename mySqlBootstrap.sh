00a20ab11cf7af7bdcdec824e7253f936bf71790
Robin Melanson <remcogold@gmail.com>
2018-05-19 13:05:26 -0400

    Update mySqlBootstrap.sh

--- mySqlBootstrap.sh
+++ mySqlBootstrap.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
-currDir=$PWD
-mySqlDir=/tmp/scripts/apps/db/MYSQL
-git clone https://github.com/RMelanson/linux-scripts-apps-db-mysql.git $mySqlDir
-cd $mySqlDir
-chmod 744 setup.sh
-./setup.sh
-cd $currDir
+mySqlCurrDir=$PWD
+
+#Set Cloning Properties
+pkg=mysql
+gitRepo="linux-scripts-apps-db-mysql.git"
+installDir="/tmp/scripts/apps/db/MYSQL"
+if [ "$1" = "ssh" ]; then
+   clone="git clone git@github.com:RMelanson/"
+else
+   clone="git clone https://github.com/RMelanson/"
+fi
+
+# Clone $pkg
+echo Executing $clone$gitRepo $installDir
+$clone$gitRepo $installDir
+
+# Setup $pkg
+cd $installDir
+. ./setup.sh
+
+cd $mySqlCurrDir
