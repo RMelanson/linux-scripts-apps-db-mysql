0f398fb7684b23d13ff6a2769c9d581fe02f47f6
Robin Melanson <remcogold@gmail.com>
2018-06-18 00:01:38 -0400

    Update mySqlBootstrap.sh

--- mySqlBootstrap.sh
+++ mySqlBootstrap.sh
@@ -12,20 +12,14 @@ yum update -y
 #INSTALL GIT
 yum install git -y
 
-if [ -z "$1" ]; then
-   branch=master
-else
-  branch=$1
-fi
-
 #Set Cloning Properties
 pkg=mysql
 gitRepo="linux-scripts-apps-db-mysql.git"
 installDir="/tmp/scripts/apps/db/MYSQL"
 if [ -f ~/.ssh/gitHub.key ]; then
-   clone="git clone -b $branch git@github.com:RMelanson/"
+   clone="git clone git@github.com:RMelanson/"
 else
-   clone="git clone -b $branch https://github.com/RMelanson/"
+   clone="git clone https://github.com/RMelanson/"
 fi
 
 # Clone $pkg
