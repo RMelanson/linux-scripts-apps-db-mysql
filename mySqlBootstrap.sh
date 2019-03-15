5035058ee2544ce511d7b909f4ebecf57bdb462b
Robin Melanson <remcogold@gmail.com>
2019-03-14 21:10:19 -0400

    Update mySqlBootstrap.sh

--- mySqlBootstrap.sh
+++ mySqlBootstrap.sh
@@ -15,8 +15,6 @@ yum install git -y
 # SETUP ENVIRONMENT AND PARAMETERS
 . ./env/setEnv.sh
 
-gitRepo="linux-scripts-apps-db-mysql.git"
-installDir="/tmp/scripts/apps/db/MYSQL"
 if [ -f ~/.ssh/gitHub.key ]; then
    clone="git clone git@github.com:RMelanson/"
 else
